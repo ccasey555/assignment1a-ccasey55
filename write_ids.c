1f220166be07ff0f2db911cab7446d69edfbd4b7
ccasey555 <cnc5478@psu.edu>
2024-09-11 18:48:49 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "ccasey55";
+    char STUDENT_ID[] = "941181651";
 
     return 0 ;
 }
